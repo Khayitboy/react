7b002f1bc468aba591492fb7294fdedcfe53f5c1
Khayitboy <ergashevx33@gmail.com>
2020-08-12 22:29:11 +0500

    state.js

---
+++ src/redux/state.js
@@ -0,0 +1,23 @@
+let state = {
+    profilePage:{
+        posts: [
+            { id: 1, message: "What's going on here?", likesCount: 5 },
+            { id: 2, message: "how are you?", likesCount: 10 },
+        ],
+    },
+    dialogsPage:{
+        dialogs: [
+            { id: 1, name: "Dimych" },
+            { id: 2, name: "User" },
+            { id: 3, name: "User 2" },
+        ],
+        messages: [
+            { id: 1, message: "Hi" },
+            { id: 2, message: "Lorem ipsum dolor sit amet consectetur adipisicing." },
+            { id: 3, message: "What's up" },
+        ]
+    }
+}
+
+
+export default state;
