aa48629b54977d006926a66bdeed9052eb306bc0
Эргашев Хайитбой Ходжирахим угли <93647@umk.uz>
2020-08-13 08:58:54 +0500

    click

--- src/components/Content/Posts/Posts.jsx
+++ src/components/Content/Posts/Posts.jsx
@@ -5,12 +5,19 @@ const Posts = (props) => {
     
     let postsElem = props.posts.map((elem) => <Post likesCount={elem.likesCount} message={elem.message} />)
     
+    let newPost = React.createRef();
+
+    let addPost = () => {
+        let text = newPost.current.value;
+        alert(text);
+    };
+
     return (
         <div className="posts">
             <div className="posts__form">
                 <form action="">
-                    <textarea id="post__form"></textarea>
-                    <button>Create Post</button>
+                    <textarea id="post__form" ref={newPost}></textarea>
+                    <button onClick={ addPost }>Create Post</button>
                 </form>
             </div>
             <h2>My posts</h2>
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -10,6 +10,12 @@ const Dialogs = (props) => {
 
     let messagesElem = props.state.messages.map((elem) => <Message message={elem.message}/>)
 
+    let messageText = React.createRef();
+
+    let btnMessage = () => {
+        let text = messageText.current.value;
+        alert(text);
+    } 
     return (
         <div className={style.dialogs_wrapper}>
             <div className={style.users}>
@@ -17,6 +23,12 @@ const Dialogs = (props) => {
             </div>
             <div className={style.dialogs}>
                 {messagesElem}
+                <div className={style.message_form}>
+                    <form action="">
+                        <textarea ref={messageText}></textarea>
+                        <button onClick={btnMessage}>Create Post</button>
+                    </form>
+                </div>
             </div>
         </div>
     );
