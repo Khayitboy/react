82476611b60c42048c5ddf394aeab445555b59ff
Эргашев Хайитбой Ходжирахим угли <93647@umk.uz>
2020-11-04 09:16:04 +0500

    auth

--- src/components/Header/HeaderContainer.js
+++ src/components/Header/HeaderContainer.js
@@ -11,7 +11,8 @@ class HeaderContainer extends React.Component {
         axios.get(`https://social-network.samuraijs.com/api/1.0/auth/me`,{
             withCredentials: true
         }).then(response => {
-            if(response.data.resultCode ===0){
+            debugger;
+            if(response.data.data.resultCode == 0){
                 let {id, email, login} = response.data.data;
                 this.props.setAuthUserData(id, email, login);
             }
